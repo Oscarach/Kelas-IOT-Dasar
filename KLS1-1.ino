33d6d1d48d7b213fe762fe4a6e85d308565dc5e1
Oscarach <89897563+Oscarach@users.noreply.github.com>
2022-07-20 17:14:12 +0700

    Update KLS1-1.ino

--- KLS1-1.ino
+++ KLS1-1.ino
@@ -2,7 +2,10 @@
  *  This sketch demonstrates how to scan WiFi networks.
  *  The API is almost the same as with the WiFi Shield library,
  *  the most obvious difference being the different file you need to include:
- */
+*/
+
+/* PENTING : SEBELUM RUNNING SOURCE CODE INI, SESUAIKAN DAHULU JENIS KIT IOT YANG DIGUNAKAN */
+
 #include <WiFi.h>
 
 void setup()
